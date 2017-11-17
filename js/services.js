0cfa81f1137c705f625d7c471fdd405da0d54bdb
tangzhi <tangzhi@asiainfo.com>
2017-11-17 20:09:24 +0800

    add LICENSE

--- js/services.js
+++ js/services.js
@@ -24,7 +24,7 @@ RunningMan.services = {
     },
 
     remove: function remove(event) {
-      console.log(event);
+      console.log($(event.target).parents('ons-list-item')[0].primaryKey);
     },
 
     clear: function clear() {
